d591fcb32d4836e3493d4a7e68f1f24b1887370d
Jarosław Kulesza <climbus@gmail.com>
2024-03-05 15:54:33 +0100

    red test getting tags form api

--- src/__tests__/Tags.spec.tsx
+++ src/__tests__/Tags.spec.tsx
@@ -1,4 +1,6 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 
 import { Tags } from "../Tags";
 
@@ -7,4 +9,36 @@ it("Renders the tags component with title and input", () => {
 
   expect(getByText("Tagi")).toBeTruthy();
   expect(getByRole("textbox", { name: "phrase" })).toBeTruthy();
+});
+
+it("When typing on input it should ask api for tags", async () => {
+  const server = setupServer(
+    http.get("http://api.example.com/tags", ({ request }) => {
+      const url = new URL(request.url);
+      const phrase = url.searchParams.get("phrase");
+
+      if (phrase !== "test") {
+        return HttpResponse.json([]);
+      }
+
+      return HttpResponse.json([
+        { id: 1, name: "test1" },
+        { id: 2, name: "test2" },
+      ]);
+    }),
+  );
+
+  server.listen();
+
+  const { getByRole, getByText } = render(<Tags />);
+  const input = getByRole("textbox", { name: "phrase" });
+
+  fireEvent.change(input, { target: { value: "test" } });
+
+  await waitFor(() => {
+    expect(getByText("test1")).toBeTruthy();
+    expect(getByText("test2")).toBeTruthy();
+  });
+
+  server.close();
 });
