1b765020d49e21848fc4d091213b7b898fd8a74b
Jarosław Kulesza <climbus@gmail.com>
2024-03-05 15:56:25 +0100

    green test for getting tags from api

--- src/Tags.tsx
+++ src/Tags.tsx
@@ -1,10 +1,30 @@
 import { Input, Stack, Typography } from "@mui/joy";
+import { useState } from "react";
+
+interface ITag {
+  id: number;
+  name: string;
+}
 
 export function Tags() {
+  const [tags, setTags] = useState<ITag[]>([]);
+
+  const handleChange = async (value: string) => {
+    const response = await fetch(`http://api.example.com/tags?phrase=${value}`);
+    const data = await response.json();
+    setTags(data);
+  };
+
   return (
     <Stack spacing={4}>
       <Typography level="h3">Tagi</Typography>
-      <Input slotProps={{ input: { "aria-label": "phrase" } }} />
+      <Input
+        slotProps={{ input: { "aria-label": "phrase" } }}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      {tags.map((tag) => (
+        <Typography key={tag.id}>{tag.name}</Typography>
+      ))}
     </Stack>
   );
 }
