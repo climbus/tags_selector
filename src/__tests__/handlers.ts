64bd49351a53fe36fc2a895b2d8f367e493412f6
Jarosław Kulesza <climbus@gmail.com>
2024-03-05 19:07:18 +0100

    some test data

--- src/__tests__/handlers.ts
+++ src/__tests__/handlers.ts
@@ -4,14 +4,24 @@ export default [
   http.get("http://api.example.com/tags", ({ request }) => {
     const url = new URL(request.url);
     const phrase = url.searchParams.get("phrase");
+    const response_data = phrase
+      ? data.filter((tag) =>
+        tag.name.toLowerCase().includes(phrase.toLowerCase()),
+      )
+      : [];
 
-    if (phrase !== "test") {
-      return HttpResponse.json([]);
-    }
-
-    return HttpResponse.json([
-      { id: 1, name: "test1" },
-      { id: 2, name: "test2" },
-    ]);
+    return HttpResponse.json(response_data);
   }),
+];
+
+const data = [
+  { id: 1, name: "test1" },
+  { id: 2, name: "test2" },
+  { id: 3, name: "hello" },
+  { id: 4, name: "world" },
+  { id: 5, name: "hello world" },
+  { id: 6, name: "ala ma kota" },
+  { id: 7, name: "react rulez" },
+  { id: 8, name: "makubma" },
+  { id: 9, name: "retro" },
 ];
