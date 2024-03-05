b9be830dbb3fd3b2ee1e8ef25414b2cb4fbd697c
Jarosław Kulesza <climbus@gmail.com>
2024-03-05 14:37:40 +0100

    red test with input

--- src/__tests__/Tags.spec.tsx
+++ src/__tests__/Tags.spec.tsx
@@ -2,7 +2,9 @@ import { render } from "@testing-library/react";
 
 import { Tags } from "../Tags";
 
-it("Renders the ... component", () => {
-  const { getByText } = render(<Tags />);
+it("Renders the tags component with title and input", () => {
+  const { getByText, getByRole } = render(<Tags />);
+
   expect(getByText("Tagi")).toBeTruthy();
+  expect(getByRole("textbox", { name: "phrase" })).toBeTruthy();
 });
